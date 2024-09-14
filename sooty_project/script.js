0dec3a39e7c40c9df9611c8cdc744acf7ce1cfc4
rosellea <garnier.eleanor@gmail.com>
2024-09-15 00:20:22 +0200

    first upload

---
+++ sooty_project/script.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.carousel_item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide(slideIndex);
+}
+
+function prevSlide() {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    showSlide(slideIndex);
+}
